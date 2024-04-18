b8d6498aa8dfb3cf63c10b4aa254c9d015871bf6
CaiorFeliciano <caiorfeliciano@gmail.com>
2024-04-17 22:22:18 -0300

    feat: add component skeleton

--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -1,14 +1,21 @@
-import { Link } from "react-router-dom";
 import { Header } from "../../components/common/Header";
 import { Grid } from "../../components/layout/Grid";
 import styles from "./Pets.module.css";
+import { Card } from "../../components/common/Card";
+import { useEffect, useState } from "react";
+import { Skeleton } from "../../components/common/Skeleton";
 
 export function Pets() {
+  const [pets, setPets] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {}, []);
   return (
     <Grid>
       <div className={styles.container}>
         <Header />
-        <Link to="/pets/20"> Ir para listagem</Link>
+        <main className={styles.list}></main>
+        <Skeleton count={4} containerClassName={styles.skeleton} />
       </div>
     </Grid>
   );
