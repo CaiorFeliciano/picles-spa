33c6d223b6bf1cc76b8421d61611cb3eb1408b0f
CaiorFeliciano <caiorfeliciano@gmail.com>
2024-04-18 22:24:59 -0300

    feat: create pets hooks and integrations with API

--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -1,21 +1,57 @@
 import { Header } from "../../components/common/Header";
 import { Grid } from "../../components/layout/Grid";
 import styles from "./Pets.module.css";
+import { useQuery } from "@tanstack/react-query";
+import { getPets } from "../../services/pets/getPets";
 import { Card } from "../../components/common/Card";
-import { useEffect, useState } from "react";
 import { Skeleton } from "../../components/common/Skeleton";
+import { Pagination } from "../../components/common/Pagination";
+import { useSearchParams } from "react-router-dom";
 
 export function Pets() {
-  const [pets, setPets] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  useEffect(() => {}, []);
+  const urlParams = {
+    page: searchParams.get("page") ? Number(searchParams.get("page")) : 1,
+  };
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["get-pets", urlParams],
+    queryFn: () => getPets(urlParams),
+  });
+
+  function changePage(page: number) {
+    setSearchParams((params) => {
+      params.set("page", String(page));
+      return params;
+    });
+  }
+
   return (
     <Grid>
       <div className={styles.container}>
         <Header />
-        <main className={styles.list}></main>
-        <Skeleton count={4} containerClassName={styles.skeleton} />
+        {isLoading && (
+          <Skeleton containerClassName={styles.skeleton} count={10} />
+        )}
+        <main className={styles.list}>
+          {data?.items?.map((pet) => (
+            <Card
+              key={pet.id}
+              href={`/pets/${pet.id}`}
+              text={pet.name}
+              thumb={pet.photo}
+            />
+          ))}
+        </main>
+
+        {data?.currentPage && (
+          <Pagination
+            currentPage={data.currentPage}
+            totalPages={data.totalPages}
+            onPageChange={(number) => changePage(number)}
+          />
+        )}
       </div>
     </Grid>
   );
