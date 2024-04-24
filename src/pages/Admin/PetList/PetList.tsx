e755c9728706c0970c9a7b8c62b026262e0aa924
CaiorFeliciano <caiorfeliciano@gmail.com>
2024-04-23 21:31:57 -0300

    feat: admin panel auth

---
+++ src/pages/Admin/PetList/PetList.tsx
@@ -0,0 +1,10 @@
+import { Panel } from "../../../components/layout/Panel";
+import styles from "./PetList.module.css";
+
+export function PetList() {
+  return (
+    <>
+      <Panel>Listagem de Pets</Panel>
+    </>
+  );
+}
---
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -0,0 +1,6 @@
+import { Panel } from "../../../components/layout/Panel";
+import styles from "./Shelter.module.css";
+
+export function Shelter() {
+  return <Panel>Formulário do Abrigo</Panel>;
+}
