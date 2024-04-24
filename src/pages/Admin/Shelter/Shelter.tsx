a6c51d3fff861c0b7480ba68905b667e71680f6c
CaiorFeliciano <caiorfeliciano@gmail.com>
2024-04-23 22:16:39 -0300

    feat: input component

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -1,6 +1,15 @@
+import { Button } from "../../../components/common/Button";
+import { Input } from "../../../components/common/Input";
 import { Panel } from "../../../components/layout/Panel";
 import styles from "./Shelter.module.css";
 
 export function Shelter() {
-  return <Panel>Formulário do Abrigo</Panel>;
+  return (
+    <Panel>
+      <form className={styles.container}>
+        <Input label="Nome" />
+        <Button type="submit">Salvar</Button>
+      </form>
+    </Panel>
+  );
 }
